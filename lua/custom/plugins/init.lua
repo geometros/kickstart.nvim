f0cdf27c68d7f95421cc05caab63ba37e564b808
Paul Christianson <pacdeveloping@gmail.com>
2024-08-30 19:27:52 -0400

    Moved custom plugins to be more easily reachable with an alias

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  --Custom keybinds
+  vim.api.nvim_set_keymap('n', '<Leader>bb', ':bnext<CR>', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<Leader>bp', ':bprevious<CR>', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<Leader>bd', ':bdelete<CR>', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<Leader>ls', ':buffers<CR>', { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap('n', '<Leader>d', ':Ex<CR>', { noremap = true, silent = true }),
+  vim.keymap.set('i', '<Leader>ee', '<Plug>(emmet-expand-abbr)', { silent = true, desc = 'Emmet expand abbreviation' }),
+}
